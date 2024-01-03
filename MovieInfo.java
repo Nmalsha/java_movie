edc2396fceb51f75bc16cff90e3d2e3cffd3e916
MalPro <malshis@yahoo.com>
2024-01-03 12:40:30 +0100

    adding files to repo

---
+++ MovieInfo.java
@@ -0,0 +1,15 @@
+public class MovieInfo {
+    public static void main(String... args) {
+
+        String movieName = "Indiana Jones and the Last Crusade";
+        boolean hasSeenMovie = true;
+        int releaseYear = 1989;
+        float imdbRating = 8.2f;
+
+        System.out.println("Movie Name (VO): " + movieName);
+        System.out.println("Have I seen the movie? " + hasSeenMovie);
+        System.out.println("Year of Release: " + releaseYear);
+        System.out.println("IMDB Rating: " + imdbRating);
+
+    }
+}
